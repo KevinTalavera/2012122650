eb3c939dde56ace919148e03a603b2b4bd57829e
Kevin Martin Huaman Talavera <Kevin Martin Huaman Talavera>
2017-04-09 20:18:30 -0500

    primer avance

---
+++ 2012122650/2012122650/2012122650/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012122650
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
